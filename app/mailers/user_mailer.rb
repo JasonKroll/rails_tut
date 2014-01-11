f85ed718df5cb120aa3de7e59f3ef2051b57bc71
Jason Kroll <Krolly@gmail.com>
2014-01-11 14:20:46 +1000

    Added registration email confirmation

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ActionMailer::Base
+  default from: "noreply.kconcepts@gmail.com"
+
+  def registration_confirmation(user)
+    @user = user
+    # can add attachment
+    # attachments["asdfasd.png"] = File.read("#{Rails.root}/public/images/asdfasd.png")
+    # might want to check if public/images is the correct location now or is it assets?
+    mail(to: user.email, subject: "Your Registered on Garraway Twits")
+  end
+end
